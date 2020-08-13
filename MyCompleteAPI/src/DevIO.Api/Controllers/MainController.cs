6ac13069313ce93539bc3b822c700e102ef4cc5b
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-08-13 11:37:15 -0300

    Update MainController.cs

--- MyCompleteAPI/src/DevIO.Api/Controllers/MainController.cs
+++ MyCompleteAPI/src/DevIO.Api/Controllers/MainController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DevIO.Business.Intefaces;
+using DevIO.Business.Notificacoes;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters.Internal;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace DevIO.Api.Controllers
@@ -10,9 +13,39 @@ namespace DevIO.Api.Controllers
     [ApiController]
     public abstract class MainController : ControllerBase
     {
+        private readonly INotificador _notificador;
+        public MainController(INotificador notificador)
+        {
+            this._notificador = notificador;
+        }
+
+        protected bool OperacaoValida()
+        {
+            return !_notificador.TemNotificacao();
+        }
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (OperacaoValida())
+            {
+                return Ok(new
+                {
+                    success = true,
+                    data = result
+                });
+            }
+
+            return BadRequest(new
+            {
+                success = false,
+                errors = _notificador.ObterNotificacoes().Select(x => x.Mensagem)
+            });
+        }
+
         protected ActionResult CustomResponse(ModelStateDictionary modelState)
         {
-
+            if (!modelState.IsValid) NotificarErroModelInvalida(modelState);
+            return CustomResponse();
         }
 
         protected void NotificarErroModelInvalida(ModelStateDictionary modelState)
@@ -21,7 +54,13 @@ namespace DevIO.Api.Controllers
             foreach (var erro in errors)
             {
                 var errorMsg = erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message;
+                NotificarErro(errorMsg);
             }
+        }
+
+        protected void NotificarErro(string mensagem)
+        {
+            _notificador.Handle(new Notificacao(mensagem));
         }
     }
 }
