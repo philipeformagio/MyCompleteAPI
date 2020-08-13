6b00f7aaab5240a08220d9c477dadb25531f4464
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-08-13 10:04:45 -0300

    custom validation

--- MyCompleteAPI/src/DevIO.Api/Controllers/MainController.cs
+++ MyCompleteAPI/src/DevIO.Api/Controllers/MainController.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace DevIO.Api.Controllers
 {
     [ApiController]
     public abstract class MainController : ControllerBase
     {
-        // notification error validate
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
 
-        // modelstate validation
+        }
 
-        // business rules validation
+        protected void NotificarErroModelInvalida(ModelStateDictionary modelState)
+        {
+            var errors = modelState.Values.SelectMany(e => e.Errors);
+            foreach (var erro in errors)
+            {
+                var errorMsg = erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message;
+            }
+        }
     }
 }
--- MyCompleteAPI/src/DevIO.Api/Startup.cs
+++ MyCompleteAPI/src/DevIO.Api/Startup.cs
@@ -38,6 +38,11 @@ namespace DevIO.Api
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true; // to use your own error custom response
+            });
+
             services.ResolveDependencies();
         }
 
