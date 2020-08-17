8ea45e6e4369dda6732d80b757bf9098bd866ce3
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-08-17 16:46:28 -0300

    CustomAuthorize

---
+++ MyCompleteAPI/src/DevIO.Api/Extentions/CustomAuthorize.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Extentions
+{
+    public class CustomAuthorization
+    {
+        public static bool ValidarClaimsUsuario(HttpContext context, string claimName, string claimValue)
+        {
+            // verifica se o usuario tem autenticacao e se tem a(s) claim(s) necessarias para acessar
+            return context.User.Identity.IsAuthenticated &&
+                   context.User.Claims.Any(c => c.Type == claimName && c.Value.Contains(claimValue));
+        }
+
+        // atributo para decorar actions e/ou controllers
+        public class ClaimsAuthorizeAttribute : TypeFilterAttribute
+        {
+            public ClaimsAuthorizeAttribute(string claimName, string claimValue) : base(typeof(RequisitoClaimFilter))
+            {
+                Arguments = new object[] { new Claim(claimName, claimValue) };
+            }
+        }
+
+        public class RequisitoClaimFilter : IAuthorizationFilter
+        {
+            private readonly Claim _claim;
+
+            public RequisitoClaimFilter(Claim claim)
+            {
+                _claim = claim;
+            }
+
+            public void OnAuthorization(AuthorizationFilterContext context)
+            {
+                if (!context.HttpContext.User.Identity.IsAuthenticated)
+                {
+                    context.Result = new StatusCodeResult(401);
+                    return;
+                }
+
+                if (!CustomAuthorization.ValidarClaimsUsuario(context.HttpContext, _claim.Type, _claim.Value))
+                {
+                    context.Result = new StatusCodeResult(403);
+                }
+            }
+        }
+    }
+}
