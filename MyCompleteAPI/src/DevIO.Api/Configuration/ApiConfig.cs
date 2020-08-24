a3f60c82fa299d00f1fbe984e41a61c3d53baf1a
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-08-24 18:21:29 -0300

    cors

--- MyCompleteAPI/src/DevIO.Api/Configuration/ApiConfig.cs
+++ MyCompleteAPI/src/DevIO.Api/Configuration/ApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,12 +20,31 @@ namespace DevIO.Api.Configuration
                 options.SuppressModelStateInvalidFilter = true; // to use your own error custom response
             });
 
-            services.AddCors(o => o.AddPolicy("Development", builder =>
+            services.AddCors(options =>
             {
-                builder.AllowAnyOrigin()
-                       .AllowAnyMethod()
-                       .AllowAnyHeader();
-            }));
+                options.AddPolicy("Development", builder =>
+                        builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+
+                options.AddPolicy("Production", builder =>
+                        builder
+                        .WithMethods("GET")
+                        .WithOrigins("http://desenvolvedor.io")
+                        .SetIsOriginAllowedToAllowWildcardSubdomains()
+                        //.WithHeaders(HeaderNames.ContentType, "x-custom-header")
+                        .AllowAnyHeader());
+
+                //options.AddDefaultPolicy(
+                //    buider =>
+                //        buider
+                //            .AllowAnyOrigin()
+                //            .AllowAnyMethod()
+                //            .AllowAnyHeader()
+                //            .AllowCredentials());
+            });
 
             return services;
         }
@@ -32,7 +52,6 @@ namespace DevIO.Api.Configuration
         public static IApplicationBuilder UseMvcConfig(this IApplicationBuilder app)
         {
             app.UseHttpsRedirection();
-            app.UseCors("Development");
             app.UseMvc();
 
             return app;
